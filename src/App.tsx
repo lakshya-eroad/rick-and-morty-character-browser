4848e9142e327c90207cc18591bb7531110e4060
lakshya.oberoi <lakshya.oberoi@eroad.com>
2024-07-15 17:49:04 +1200

    chore: center title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">Rick and Morty Characters</h1>
+        <h1 className="text-3xl font-bold mb-4 text-center">
+          Rick and Morty Characters
+        </h1>
         <Routes>
           <Route path="/" element={<CharacterList />} />
           <Route path="/character/:id" element={<CharacterModal />} />
