8538b667afaf43ebf7b2630c0220ea3bd130e088
lakshya.oberoi <lakshya.oberoi@eroad.com>
2024-07-15 16:58:31 +1200

    package: tailwind and config

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,20 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import { ApolloProvider } from "@apollo/client";
 
 import App from "./App";
 import client from "./client/client";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import "./tailwind.css";
 
-const container = document.getElementById("app");
-
-const root = createRoot(container!);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
